e0348a47dd46b55e160aefbe364e2968ca884d6f
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-09-27 18:36:11 +0530

    edit for placing map component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BusStopsDelhi from './containers/BusStopsDelhi';
+
+function App() {
+  return (
+    <div className="App">
+      <BusStopsDelhi />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/BusStopsDelhi.js
@@ -0,0 +1,60 @@
+import esriConfig from '@arcgis/core/config';
+import Map from '@arcgis/core/Map';
+import MapView from '@arcgis/core/views/MapView';
+import PictureMarkerSymbol from '@arcgis/core/symbols/PictureMarkerSymbol';
+import GeoJSONLayer from '@arcgis/core/layers/GeoJSONLayer';
+import { useEffect } from 'react';
+
+const BusStopsDelhi = () => {
+    useEffect(() => {
+
+        //API key
+        esriConfig.apiKey = "AAPK654ada81dfb94a41bebd71ff4d8f2819nvY5Wma3Hge2PT9Uy6XB14bgnNo_q1zEGBCWwTmloU6F1qtvgkiSTYiSHFlYGT5G";
+        
+        //custom svg marker for bus stops
+        const picSymbol = new PictureMarkerSymbol({
+            "url": './directions_bus.svg',
+            "width":20,
+            "height":20,
+        });
+
+        //renders an on-click tooltip
+        const template = {
+            title: "{name} Bus Stop",
+            content: "",
+        };
+
+        //renders a bus stop icon for each bus stop
+        const renderer = {
+            type: "simple",
+            field: "name",
+            symbol: picSymbol, //bus icon
+        };
+
+        //contains data for the layer to render all bus stops
+        const geojsonLayer = new GeoJSONLayer({
+            url: "./busStops.json", //data
+            popupTemplate: template, //tooltip
+            renderer: renderer, //marker
+        });
+
+        //create a map object
+        const map = new Map({
+            basemap: "arcgis-navigation", // Basemap layer service
+            layers: [geojsonLayer] //bus stops
+        });
+
+        //customize how the map should look
+        // eslint-disable-next-line no-unused-vars
+        const view = new MapView({
+            map: map, //map object
+            center: [77.216721,28.644800], //cooordinates of the default center of the map
+            zoom: 11, //default zoom level
+            container: "view" //where to place the map
+        });
+    }, []);
+
+    return <div id="view" style={{width:"100%", height:"100vh"}}></div>
+}
+
+export default BusStopsDelhi;
