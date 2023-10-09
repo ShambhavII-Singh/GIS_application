fcf8079c7d24e203aec0ae8385f85f71136a354d
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-10-09 19:17:13 +0530

    create graph class

--- src/assets/findStops.js
+++ src/assets/findStops.js
@@ -1,2 +1,30 @@
 import data from './busStops.json';
 
+// graph class with its methods
+class Graph {
+    // graph constructor
+    constructor() {
+        this.adjacencyList = {};
+    }
+    // method to add a vertex to the graph
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = [];
+        }
+    }
+    // method to add an edge to the graph
+    addEdge(source, destination) {
+        this.addVertex(source);
+        this.addVertex(destination);
+        // this graph is directed form origin to destination
+        this.adjacencyList[source].push(destination);
+    }
+    // methos to remove an edge from the graph
+    removeEdge(source, destination) {
+        this.adjacencyList[source] = this.adjacencyList[source].filter(vertex => vertex !== destination);
+    }
+    // method to return graph json object directly
+    graph() {
+        return this.adjacencyList;
+    }
+}
