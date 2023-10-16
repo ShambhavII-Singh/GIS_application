d70884e475edbd0ae184d6afbf5a41f8476b20c1
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-10-16 22:35:25 +0530

    create route layer

---
+++ src/assets/routingService.js
@@ -0,0 +1,10 @@
+import RouteParameters from '@arcgis/core/rest/support/RouteParameters';
+import FeatureSet from '@arcgis/core/rest/support/FeatureSet.js';
+import * as route from "@arcgis/core/rest/route.js";
+import Graphic from '@arcgis/core/Graphic';
+
+const routingService = () => {
+
+}
+
+export default routingService;
