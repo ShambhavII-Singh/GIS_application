58f3050e73bd68d44776902ac5106ad715347c65
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-10-19 12:54:09 +0530

    moved to components folder

---
+++ src/components/routingService.js
@@ -0,0 +1,74 @@
+import RouteParameters from '@arcgis/core/rest/support/RouteParameters';
+import FeatureSet from '@arcgis/core/rest/support/FeatureSet.js';
+import * as route from "@arcgis/core/rest/route.js";
+import Graphic from '@arcgis/core/Graphic';
+import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
+import findRoutes from './findStops';
+
+const routingService = (map,view,origin,destination) => {
+    // url to the mapping and routing service
+    const routeUrl = "https://route-api.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World";
+
+    // create the origin and destination by clicking
+    const stops = findRoutes(origin,destination);
+    const routeLayer = new GraphicsLayer();
+    map.layers.push(routeLayer)
+    const addGraphic = (type, point) => {
+        const graphic = new Graphic({
+            symbol: {
+                type: "simple-marker",
+                color: (type === "origin") ? 
+                "white" : (type === "destination") ?
+                "black" : (type === "intermediate") ? 
+                "red" : null,
+                size: "8px"
+            },
+            geometry: point
+        });
+        view.graphics.add(graphic);
+        return graphic;
+    }
+
+    function getCleanRoute(origin,destination,parameterOrigin,parameterDestination) {
+        const routeParams = new RouteParameters({
+            apiKey: "AAPK654ada81dfb94a41bebd71ff4d8f2819nvY5Wma3Hge2PT9Uy6XB14bgnNo_q1zEGBCWwTmloU6F1qtvgkiSTYiSHFlYGT5G",
+            stops: new FeatureSet(),
+            returnDirections: true
+        });
+
+        routeParams.stops.features.push(addGraphic(parameterOrigin, origin));
+        routeParams.stops.features.push(addGraphic(parameterDestination, destination));
+
+        route.solve(routeUrl, routeParams)
+        .then(function(data) {
+            data.routeResults.forEach(function(result) {
+                result.route.symbol = {
+                    type: "simple-line",
+                    color: [5, 150, 255],
+                    width: 3
+                };
+                routeLayer.add(result.route);
+            });
+            view.extent(routeLayer.extent);
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+    }
+
+    
+    for (let i = 0; i < stops.length-1; i++) {
+        let parameterDestination,parameterOrigin;
+        parameterDestination = "intermediate";
+        parameterOrigin = "intermediate";
+        if (i===0) {
+            parameterOrigin = "origin";
+        }
+        if (i+1===stops.length-1) {
+            parameterDestination = "destination";
+        }
+        getCleanRoute(stops[i],stops[i+1],parameterOrigin,parameterDestination); // Call the route service
+    }
+}
+
+export default routingService;
