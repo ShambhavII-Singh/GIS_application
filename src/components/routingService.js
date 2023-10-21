3a66990b4629de14011f229bce6a1cf8145d3ac5
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-10-21 23:48:25 +0530

    dialog box for route statistics 4

--- src/components/routingService.js
+++ src/components/routingService.js
@@ -56,7 +56,7 @@ const routingService = (view,origin,destination) => {
             var routeLength = (Number(window.localStorage.getItem("routeLength"))+data.routeResults[0].route.attributes["Total_Kilometers"]).toString();
             
             window.localStorage.setItem("routeLength",routeLength);
-            console.log(window.localStorage.getItem("routeLength"))
+            
             data.routeResults.forEach(function(result) {             
                 result.route.symbol = {
                     type: "simple-line",
