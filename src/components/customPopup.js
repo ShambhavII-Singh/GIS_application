57caa8cfcf7b084dc33a125610fed7d36ef76022
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-10-22 23:27:06 +0530

    edit default popup

--- src/components/customPopup.js
+++ src/components/customPopup.js
@@ -12,9 +12,6 @@ const customPopup = (view,geojsonLayer) => {
                         <p>General instructions:</p>
                         <ul>
                             <li>Click on the bus icons for more information</li>
-                            <li>Drag to navigate through the map</li>
-                            <li>Use two fingers to zoom in and out</li>
-                            <li>Click on any bus icon to search by name</li>
                             <li>Use the drop-downs given below for calculating the cleanest route</li>
                         </ul>
                     </div>`,
