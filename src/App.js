e260ce0b063c4c89d60c34bd27c90d34d835ce5d
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-11-04 18:32:20 +0530

    create hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import { Navbar } from './containers';
+import { Navbar, Hero } from './containers';
 import BusStopsDelhi from './containers/BusStopsDelhi';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
       <BusStopsDelhi />
     </div>
   );
