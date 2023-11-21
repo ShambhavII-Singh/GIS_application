f7b954b671ded65ca259cf61122f868fda87af51
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-11-21 13:33:31 +0530

    create footer

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,47 +1,13 @@
 import React from 'react';
 import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
-import FooterOverlay from '../../components/About/FooterOverlay/FooterOverlay';
-import Newsletter from '../../components/About/Newsletter/Newsletter';
 import './About.css';
 
 const About  = () => (
-  <div className="app__footer section__padding" id="login">
-    <FooterOverlay />
-    <Newsletter />
-
-    <div className="app__footer-links">
-      <div className="app__footer-links_contact">
-        <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
-        <p className="p__opensans">+1 212-344-1230</p>
-        <p className="p__opensans">+1 212-555-1230</p>
-      </div>
-
-      <div className="app__footer-links_logo">
-        {/* <a href="#about"><img src={images.gericht} alt="footer_logo"/></a> */}
-        <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
-        {/* <img src={images.spoon} alt='spoon' className="spoon__img" style={{ marginTop: 15 }} /> */}
-        <div className="app__footer-links_icons">
-          <FiFacebook />
-          <FiTwitter />
-          <FiInstagram />
-        </div>
-      </div>
-
-      <div className="app__footer-links_work">
-        <h1 className="app__footer-headtext">Working Hours</h1>
-        <p className="p__opensans">Monday-Friday:</p>
-        <p className="p__opensans">08:00 am - 12:00 am</p>
-        <p className="p__opensans">Saturday-Sunday:</p>
-        <p className="p__opensans">07:00 am - 11:00 pm</p>
-      </div>
+  <footer className='footer'>
+    <div className="footer-content">
+      <p>Made with ❤️ by <a href="https://github.com/ShambhavII-Singh">Shambhavi</a></p>
     </div>
-
-    <div className="footer__copyright">
-      <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
-    </div>
-
-  </div>
+  </footer>
 );
 
 export default About;
